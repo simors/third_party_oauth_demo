796ffd5f463105a18334030cf3fe2b81776b818a
yang24201 <yang24201@126.com>
2018-01-06 17:43:46 +0800

    add lvyii_third_party_oauth middleware

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var LYOAUTH = require('lvyii_third_party_oauth')
+
+var oauthRedis = {
+  redis_url: '120.77.220.234',
+  redis_port: '6379',
+  redis_db: '12',
+  redis_auth: 'Simors2017',
+}
+
+var wechatOauthCfg = {
+  channel: 'wechat',
+  serverUrl: 'http://hudong-dev.leanapp.cn/wechatOauth',
+  appId: 'wx34ac208b373814d2',
+  appSecret: 'd121a1921db870cc3e37f148cb7cc257',
+}
+
+LYOAUTH.init({
+  media: 'redis',
+  mediaCfg: oauthRedis,
+  thirdParty: [wechatOauthCfg],
+})
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +42,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(LYOAUTH.express())
 
 app.use('/', index);
 app.use('/users', users);
